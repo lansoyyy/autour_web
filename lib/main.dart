f5fcaafcdc03d88d35d8f3e9bb8dafc523226338
Lans12345 <olanalans12345@gmail.com>
2025-08-27 08:06:01 +0800

    added image picking func

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AuTour',
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
